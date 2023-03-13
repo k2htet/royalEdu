bf0875cf2d17fc15e337c1e8591d7c5672f4a101
KoHtet <kokoh6329@gmail.com>
2023-03-13 22:43:58 +0630

    start student dashboard card 1.1

--- app/(auth-user)/student/dashboard/page.jsx
+++ app/(auth-user)/student/dashboard/page.jsx
@@ -1,4 +1,6 @@
 import { createClient } from "@/app/utils/supabase-server";
+import DashboardCard from "../../components/DashboardCard";
+import EnrollCourseCard from "../../components/EnrollCourseCard";
 
 const Dashboard = async () => {
   const supabase = createClient();
@@ -8,8 +10,20 @@ const Dashboard = async () => {
 
   return (
     <div className="flex-1 bg-background flex items-center justify-center">
-      <div className="container px-4 mx-auto grid grid-cols-1 lg:grid-cols-2 py-4">
-        <h1 className="text-primary">မကြာမီလာမည်။</h1>
+      <div className="container px-4 mx-auto grid grid-cols-1 lg:grid-cols-2 py-4 gap-4">
+        <div className="space-y-3">
+          <DashboardCard title="Enroll Courses" items="2">
+            <EnrollCourseCard
+              title="N5 Full Package"
+              classes="80 Classes"
+              url="/"
+            />
+          </DashboardCard>
+          <DashboardCard />
+        </div>
+        <div>
+          <DashboardCard />
+        </div>
       </div>
     </div>
   );
