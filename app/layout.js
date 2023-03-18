21669274677831ce20ee604e27904176a317263e
KoHtet <kokoh6329@gmail.com>
2023-03-18 22:22:05 +0630

    student-branch merge to main

--- app/layout.js
+++ app/layout.js
@@ -47,21 +47,11 @@ export default async function RootLayout({ children }) {
       .single();
 
     if (error) {
-<<<<<<< HEAD
       await supabase.from("students").insert({
         id: user.id,
         name: user.user_metadata.username,
         email: user.email,
       });
-=======
-      await supabase
-        .from("students")
-        .insert({
-          id: user.id,
-          name: user.user_metadata.username,
-          email: user.email,
-        });
->>>>>>> student-dashboard
     }
   }
 
